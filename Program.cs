ed696c3ae0e4ed3f19c5042113da4ffda633b5b8
naren004 <narendhran.m92@gmail.com>
2025-06-04 12:15:20 +0530

    cors policy added

--- Program.cs
+++ Program.cs
@@ -1,22 +1,41 @@
-using BookApi.Models;
+﻿using BookApi.Models;
 using BookApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure settings from appsettings.json
+// Load MongoDB settings
 builder.Services.Configure<BookstoreDatabaseSettings>(
     builder.Configuration.GetSection("MongoDB"));
 
+// Register services
 builder.Services.AddSingleton<BooksService>();
 
+// Enable CORS for Blazor
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient",
+        policy => policy
+            .WithOrigins("https://localhost:7291") // Blazor WebAssembly URL
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+// Add other services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Use Swagger in development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// 👇 Use CORS before any endpoints or authorization
+app.UseCors("AllowBlazorClient");
 
 app.UseAuthorization();
 
--- obj/Debug/net8.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c90744b39f352824c79471f9fd56623e9396028c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
